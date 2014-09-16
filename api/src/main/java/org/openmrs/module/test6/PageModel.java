38953087d069655264d7166baea2f406bbf47f6d
dkayiwa <dkayiwa@openmrs.org>
2014-09-17 01:21:46 +0300

    Adding modules files to the repository

---
+++ api/src/main/java/org/openmrs/module/test6/PageModel.java
@@ -0,0 +1,23 @@
+package org.openmrs.module.test6;
+
+
+public class PageModel {
+	
+	private String name;
+
+	
+    /**
+     * @return the name
+     */
+    public String getName() {
+    	return name;
+    }
+
+	
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+    	this.name = name;
+    }
+}
